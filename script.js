f7557c3ede362425be629f86a855939b884a788f
GustavoHMoraes <gustavo.rocillo@etec.sp.gov.br>
2023-03-28 15:44:11 -0300

    nova versao.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function imagemAleatoria() {
-    var numeroAleatorio = Math.floor(Math.random() * 10) + 1;
-    var novaImagem = "imgoverwatch-" + numeroAleatorio + ".jpg";
+    var numeroAleatorio = Math.trunc(Math.random() * 10);
+    var novaImagem = "img/imgoverwatch-" + numeroAleatorio + ".jpg";
     document.getElementById("imagem").src = novaImagem;
   }
-  
-  document.getElementById("imagem").addEventListener("click", imagemAleatoria);
